80e5bddccb16a5d26a4bf59f5833a8114046de7c
gdjohn4s <giovanni@exifly.it>
2023-09-26 00:47:51 +0200

    feat: created cli.py file for handling cli commands

--- src/cli.py
+++ src/cli.py
@@ -9,8 +9,8 @@ class Cli:
         parser = argparse.ArgumentParser(description="Shell GPT Pippo")
         parser.add_argument(Args.SETAPIKEY, type=str, help="set openai api key")
         args = parser.parse_args()
-        print(args)
-        assert not args, "Please provide an argument, use -h or --help to get help."
+        if not args:
+            print("Please provide an argument, use -h or --help to get help.")
 
     def __add_to_config(self, config) -> None:
         pass
