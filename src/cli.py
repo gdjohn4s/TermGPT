30fad74822f31f08353d9c20db1a5ee7ef1e321e
gdjohn4s <giovanni@exifly.it>
2023-09-27 16:55:30 +0200

    edit: updated description message

--- src/cli.py
+++ src/cli.py
@@ -6,7 +6,7 @@ class Args:
 
 class Cli:
     def __init__(self) -> None:
-        parser = argparse.ArgumentParser(description="Shell GPT Pippo")
+        parser = argparse.ArgumentParser(description="TermGPT tool made by gdjohn4s")
         parser.add_argument(Args.SETAPIKEY, type=str, help="set openai api key")
         args = parser.parse_args()
         if not args:
